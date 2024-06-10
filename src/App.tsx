1a958c65fb1f135af1c93e39df357d1e77605acc
Muhammad Qasim <qasimirshad0@gmail.com>
2024-06-11 03:08:49 +0500

    main layout half code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { UISwitch } from "./components";
+import NavCard from "./components/NavCard";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -20,13 +21,24 @@ function App() {
   };
 
   return (
-    <div className="w-full h-full flex flex-col p-16 gap-4">
+    <div className="bg-background-light dark:bg-background-dark w-full h-full flex flex-col gap-4 bg-[url('./assets/images/background.png')]"
+      style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}>
       <UISwitch
-        label="Dark Theme"
+        label=""
         checked={isDarkMode}
         onChange={toggleTheme}
       />
-      <div className="bg-orange-1 dark:bg-yellow-1">Home</div>
+      {/* <div className="bg-orange-1 dark:bg-yellow-1">Home</div> */}
+      <div className="h-84vh mx-0 md:mx-[3vh] lg:mx-[6vh] md:my-[8vw]"
+        style={{
+          position:'relative',
+        }}>
+        <NavCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/NavCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavCard = () => {
+  return (
+    <div className='h-full border border-gray-200 bg-background-dark bg-opacity-70'>NavCard</div>
+  )
+}
+
+export default NavCard
